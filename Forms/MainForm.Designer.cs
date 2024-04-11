875607921f6f2e17db11f998dbe85f4bda6c05ea
Tsuniti <Tsuniti00@gmail.com>
2024-04-11 17:17:40 +0300

    (update) MainForm

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,45 +1,193 @@
-﻿namespace Wall
+﻿namespace Wall;
+using Wall.Components;
+
+partial class MainForm
 {
-    partial class MainForm
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            SuspendLayout();
-            // 
-            // MainForm
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "MainForm";
-            Text = "MainForm";
-            ResumeLayout(false);
-        }
-
-        #endregion
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        AllMessagesPanel = new Panel();
+        WriteMessagePanel = new Panel();
+        SendButton = new Button();
+        NewMessageTextBox = new TextBox();
+        TitleBarPanel = new TitleBarPanel();
+        UsernameLabel = new Label();
+        LogInButton = new Button();
+        SignUpButton = new Button();
+        LogOutLinkLabel = new LinkLabel();
+        WriteMessagePanel.SuspendLayout();
+        SuspendLayout();
+        // 
+        // AllMessagesPanel
+        // 
+        AllMessagesPanel.AutoScroll = true;
+        AllMessagesPanel.BackColor = Color.FromArgb(13, 17, 23);
+        AllMessagesPanel.BorderStyle = BorderStyle.FixedSingle;
+        AllMessagesPanel.Location = new Point(12, 109);
+        AllMessagesPanel.Name = "AllMessagesPanel";
+        AllMessagesPanel.Size = new Size(663, 450);
+        AllMessagesPanel.TabIndex = 0;
+        // 
+        // WriteMessagePanel
+        // 
+        WriteMessagePanel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+        WriteMessagePanel.BackColor = Color.FromArgb(13, 17, 23);
+        WriteMessagePanel.Controls.Add(SendButton);
+        WriteMessagePanel.Controls.Add(NewMessageTextBox);
+        WriteMessagePanel.Location = new Point(12, 568);
+        WriteMessagePanel.Name = "WriteMessagePanel";
+        WriteMessagePanel.Size = new Size(651, 125);
+        WriteMessagePanel.TabIndex = 1;
+        // 
+        // SendButton
+        // 
+        SendButton.BackColor = Color.FromArgb(33, 38, 45);
+        SendButton.Enabled = false;
+        SendButton.FlatStyle = FlatStyle.Popup;
+        SendButton.ForeColor = Color.White;
+        SendButton.Location = new Point(571, 24);
+        SendButton.Name = "SendButton";
+        SendButton.Size = new Size(72, 77);
+        SendButton.TabIndex = 2;
+        SendButton.Text = "Send";
+        SendButton.UseVisualStyleBackColor = false;
+        SendButton.Click += sendButton_Click;
+        // 
+        // NewMessageTextBox
+        // 
+        NewMessageTextBox.BackColor = Color.FromArgb(22, 27, 34);
+        NewMessageTextBox.BorderStyle = BorderStyle.FixedSingle;
+        NewMessageTextBox.Enabled = false;
+        NewMessageTextBox.Font = new Font("Segoe UI", 9F);
+        NewMessageTextBox.ForeColor = Color.White;
+        NewMessageTextBox.Location = new Point(3, 24);
+        NewMessageTextBox.Multiline = true;
+        NewMessageTextBox.Name = "NewMessageTextBox";
+        NewMessageTextBox.Size = new Size(562, 77);
+        NewMessageTextBox.TabIndex = 1;
+        NewMessageTextBox.KeyDown += NewMessageTextBox_KeyDown;
+        // 
+        // TitleBarPanel
+        // 
+        TitleBarPanel.BackColor = Color.FromArgb(9, 9, 9);
+        TitleBarPanel.Dock = DockStyle.Top;
+        TitleBarPanel.Location = new Point(0, 0);
+        TitleBarPanel.Name = "TitleBarPanel";
+        TitleBarPanel.Size = new Size(675, 44);
+        TitleBarPanel.TabIndex = 2;
+        // 
+        // UsernameLabel
+        // 
+        UsernameLabel.AutoSize = true;
+        UsernameLabel.Font = new Font("Segoe UI", 14F);
+        UsernameLabel.ForeColor = Color.White;
+        UsernameLabel.Location = new Point(16, 59);
+        UsernameLabel.Name = "UsernameLabel";
+        UsernameLabel.Size = new Size(177, 32);
+        UsernameLabel.TabIndex = 3;
+        UsernameLabel.Text = "UsernameLabel";
+        // 
+        // LogInButton
+        // 
+        LogInButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(8, 34, 86);
+        LogInButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(4, 19, 48);
+        LogInButton.FlatStyle = FlatStyle.Flat;
+        LogInButton.Font = new Font("Segoe UI", 10F);
+        LogInButton.ForeColor = Color.White;
+        LogInButton.Location = new Point(561, 62);
+        LogInButton.Name = "LogInButton";
+        LogInButton.Size = new Size(94, 41);
+        LogInButton.TabIndex = 4;
+        LogInButton.Text = "Log in";
+        LogInButton.UseVisualStyleBackColor = true;
+        LogInButton.Click += LogInButton_Click;
+        // 
+        // SignUpButton
+        // 
+        SignUpButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(8, 34, 86);
+        SignUpButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(4, 19, 48);
+        SignUpButton.FlatStyle = FlatStyle.Flat;
+        SignUpButton.Font = new Font("Segoe UI", 10F);
+        SignUpButton.ForeColor = Color.White;
+        SignUpButton.Location = new Point(461, 62);
+        SignUpButton.Name = "SignUpButton";
+        SignUpButton.Size = new Size(94, 41);
+        SignUpButton.TabIndex = 5;
+        SignUpButton.Text = "Sign up";
+        SignUpButton.UseVisualStyleBackColor = true;
+        SignUpButton.Click += SignUpButton_Click;
+        // 
+        // LogOutLinkLabel
+        // 
+        LogOutLinkLabel.ActiveLinkColor = Color.WhiteSmoke;
+        LogOutLinkLabel.AutoSize = true;
+        LogOutLinkLabel.LinkColor = Color.Gray;
+        LogOutLinkLabel.Location = new Point(579, 73);
+        LogOutLinkLabel.Name = "LogOutLinkLabel";
+        LogOutLinkLabel.Size = new Size(60, 20);
+        LogOutLinkLabel.TabIndex = 6;
+        LogOutLinkLabel.TabStop = true;
+        LogOutLinkLabel.Text = "Log out";
+        LogOutLinkLabel.Visible = false;
+        LogOutLinkLabel.VisitedLinkColor = Color.Gray;
+        LogOutLinkLabel.LinkClicked += LogOutLinkLabel_LinkClicked;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        AutoScroll = true;
+        BackColor = Color.FromArgb(1, 4, 9);
+        ClientSize = new Size(675, 705);
+        Controls.Add(LogOutLinkLabel);
+        Controls.Add(SignUpButton);
+        Controls.Add(LogInButton);
+        Controls.Add(UsernameLabel);
+        Controls.Add(WriteMessagePanel);
+        Controls.Add(AllMessagesPanel);
+        Controls.Add(TitleBarPanel);
+        FormBorderStyle = FormBorderStyle.None;
+        Name = "MainForm";
+        Text = "MainForm";
+        Load += MainForm_Load;
+        WriteMessagePanel.ResumeLayout(false);
+        WriteMessagePanel.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Panel AllMessagesPanel;
+    private Panel WriteMessagePanel;
+    private TextBox NewMessageTextBox;
+    private Button SendButton;
+    private TitleBarPanel TitleBarPanel;
+    private Label UsernameLabel;
+    private Button LogInButton;
+    private Button SignUpButton;
+    private LinkLabel LogOutLinkLabel;
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,10 +1,173 @@
+using Wall.Databases;
+using Wall.Entities;
+using Wall.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Windows.Forms;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Wall.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
 namespace Wall
 {
     public partial class MainForm : Form
     {
+
+        PublicationPanel? PreviousPublicationPanel;
+
+        AuthorizationForm? AuthorizationForm;
+        public ChatUser? AuthorizedUser;
+        //List<Publication> Publications;
+        //public UserToPublicationsDB db = new UserToPublicationsDB();
+
+        public HubConnection MyHubConnection;
+
         public MainForm()
         {
+
             InitializeComponent();
+
+
+            MyHubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7005/chat")
+                .Build();
+            //hubConnection.Closed += HubConnection_Closed;
+
+            MyHubConnection.On<string, string, DateTime>("Receive", (user, message, date) =>
+            {
+
+                Task.Run(() =>
+                {
+                    Invoke(new Action(() =>
+                    {
+                        ShowMessage(user, message, date);
+                    }));
+                });
+            });
+
+
+        }
+
+        private async Task HubConnection_Closed(Exception? arg)
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await MyHubConnection.StartAsync();
+        }
+        private void ShowMessage(string user, string message, DateTime? date = null)
+        {
+            bool IsScrollBottom = (AllMessagesPanel.VerticalScroll.Value + AllMessagesPanel.VerticalScroll.LargeChange >= AllMessagesPanel.VerticalScroll.Maximum);
+            var publicationPanel =
+                 new PublicationPanel(user, message, date);
+            if (PreviousPublicationPanel != null)
+            {
+                publicationPanel.Location = new System.Drawing.Point(12,
+                    PreviousPublicationPanel.Location.Y + PreviousPublicationPanel.Height + 3);
+            }
+            else
+            {
+                publicationPanel.Location = new System.Drawing.Point(12, 340);
+            }
+            PreviousPublicationPanel = publicationPanel;
+
+            AllMessagesPanel.Controls.Add(publicationPanel);
+
+            if(IsScrollBottom)
+            {
+                AllMessagesPanel.ScrollControlIntoView(publicationPanel);
+            }
+
+        }
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            TitleBarPanel.ParentForm = this;
+
+            try
+            {
+                // подключемся к хабу
+                await MyHubConnection.StartAsync();
+                ShowMessage("System", "Connection started.");
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("System", ex.Message);
+            }
+
+            try
+            {
+                await MyHubConnection.InvokeAsync("ReceiveAll");
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("System", ex.Message);
+            }
+
+            if (AuthorizedUser != null)
+            {
+                UsernameLabel.Text = "Guest";
+                NewMessageTextBox.Enabled = false;
+            }
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            if (NewMessageTextBox.Text != string.Empty)
+            {
+
+
+                try
+                {
+                    await MyHubConnection.InvokeAsync("Send", AuthorizedUser, NewMessageTextBox.Text);
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage("System", ex.Message);
+                }
+            NewMessageTextBox.Text = String.Empty;
+            }
+        }
+
+        private void LogInButton_Click(object sender, EventArgs e)
+        {
+            AuthorizationForm = new AuthorizationForm("Log in");
+            AuthorizationForm.ParentForm = this;
+            AuthorizationForm.ShowDialog();
+        }
+
+        private void SignUpButton_Click(object sender, EventArgs e)
+        {
+            AuthorizationForm = new AuthorizationForm();
+            AuthorizationForm.ParentForm = this;
+            AuthorizationForm.ShowDialog();
+        }
+
+        private void LogOutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            AuthorizedUser = null;
+            UsernameLabel.Text = String.Empty;
+            SignUpButton.Visible = true;
+            LogInButton.Visible = true;
+            LogOutLinkLabel.Visible = false;
+            NewMessageTextBox.Enabled = false;
+            SendButton.Enabled = false;
+        }
+        public void UserLogIn(string username)
+        {
+            UsernameLabel.Text = username;
+            NewMessageTextBox.Enabled = true;
+            SendButton.Enabled = true;
+            LogInButton.Visible = false;
+            SignUpButton.Visible = false;
+            LogOutLinkLabel.Visible = true;
+        }
+
+        private void NewMessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                sendButton_Click(this, new EventArgs());
+            }
+
         }
     }
 }
