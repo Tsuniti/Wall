333d1483ba4b8f55963ca50f9f0220d804d33ddd
Tsuniti <Tsuniti00@gmail.com>
2024-04-11 17:15:32 +0300

    (add) TitleBarPanel

---
+++ Components/TitleBarPanel.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Wall.Components;
+
+partial class TitleBarPanel
+{
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TitleBarPanel));
+        MinimizeButton = new Button();
+        CloseButton = new Button();
+        SuspendLayout();
+        // 
+        // MinimizeButton
+        // 
+        MinimizeButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+        MinimizeButton.FlatAppearance.BorderSize = 0;
+        MinimizeButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(58, 58, 58);
+        MinimizeButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
+        MinimizeButton.FlatStyle = FlatStyle.Flat;
+        MinimizeButton.Image = (Image)resources.GetObject("MinimizeButton.Image");
+        MinimizeButton.Location = new Point(439, 0);
+        MinimizeButton.Name = "MinimizeButton";
+        MinimizeButton.Size = new Size(56, 44);
+        MinimizeButton.TabIndex = 1;
+        MinimizeButton.UseVisualStyleBackColor = true;
+        MinimizeButton.Click += MinimizeButton_Click;
+        // 
+        // CloseButton
+        // 
+        CloseButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+        CloseButton.FlatAppearance.BorderSize = 0;
+        CloseButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(148, 19, 32);
+        CloseButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(205, 26, 43);
+        CloseButton.FlatStyle = FlatStyle.Flat;
+        CloseButton.Image = (Image)resources.GetObject("CloseButton.Image");
+        CloseButton.Location = new Point(492, 0);
+        CloseButton.Name = "CloseButton";
+        CloseButton.Size = new Size(56, 44);
+        CloseButton.TabIndex = 0;
+        CloseButton.UseVisualStyleBackColor = true;
+        CloseButton.Click += CloseButton_Click;
+        // 
+        // TitleBarPanel
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.FromArgb(9, 9, 9);
+        Controls.Add(CloseButton);
+        Controls.Add(MinimizeButton);
+        Name = "TitleBarPanel";
+        Size = new Size(548, 44);
+        MouseDown += TitleBarPanel_MouseDown;
+        ResumeLayout(false);
+    }
+
+    #endregion
+    private Button MinimizeButton;
+    private Button CloseButton;
+}
---
+++ Components/TitleBarPanel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wall.Components;
+
+public partial class TitleBarPanel : UserControl
+{
+
+    [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+    private extern static bool ReleaseCapture();
+    [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+    private extern static int SendMessage(System.IntPtr hWnd,
+                                int Msg, int wParam, int lParam);
+
+    public Form ParentForm { get; set; }
+    public TitleBarPanel()
+    {
+        InitializeComponent();
+    }
+
+    private void TitleBarPanel_MouseDown(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+        {
+            ReleaseCapture();
+            SendMessage(ParentForm.Handle, 0x112, 0xf012, 0);
+        }
+    }
+
+    private void CloseButton_Click(object sender, EventArgs e)
+    {
+        //Environment.Exit(0);
+        ParentForm.Close();
+    }
+
+    private void MinimizeButton_Click(object sender, EventArgs e)
+    {
+        ReleaseCapture();
+        SendMessage(ParentForm.Handle, 0x112, 0xf020, 0);
+    }
+    public void HideMinimizeButton()
+    {
+        MinimizeButton.Visible = false;
+    }
+}
