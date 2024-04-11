87ac8a621b5228fc0bd10ad9dd6f25f57768076c
Tsuniti <Tsuniti00@gmail.com>
2024-04-11 17:17:26 +0300

    (add) AuthorizationForm

---
+++ Forms/AuthorizationForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Wall.Forms
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TitleBarPanel = new Components.TitleBarPanel();
+            SubmitButton = new Button();
+            UsernameTextBox = new TextBox();
+            PasswordTextBox = new TextBox();
+            TopLabel = new Label();
+            RepeatPasswordTextBox = new TextBox();
+            ErrorLabel = new Label();
+            LogInLinkLabel = new LinkLabel();
+            BorderRightPanel = new Panel();
+            BorderLeftPanel = new Panel();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // TitleBarPanel
+            // 
+            TitleBarPanel.BackColor = Color.FromArgb(9, 9, 9);
+            TitleBarPanel.Dock = DockStyle.Top;
+            TitleBarPanel.Location = new Point(0, 0);
+            TitleBarPanel.Name = "TitleBarPanel";
+            TitleBarPanel.Size = new Size(390, 42);
+            TitleBarPanel.TabIndex = 0;
+            // 
+            // SubmitButton
+            // 
+            SubmitButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            SubmitButton.FlatStyle = FlatStyle.Flat;
+            SubmitButton.Font = new Font("Segoe UI", 11F);
+            SubmitButton.ForeColor = Color.White;
+            SubmitButton.Location = new Point(12, 369);
+            SubmitButton.Name = "SubmitButton";
+            SubmitButton.Size = new Size(366, 41);
+            SubmitButton.TabIndex = 1;
+            SubmitButton.Text = "Sign up";
+            SubmitButton.UseVisualStyleBackColor = true;
+            SubmitButton.Click += SignUpButton_Click;
+            // 
+            // UsernameTextBox
+            // 
+            UsernameTextBox.BackColor = Color.FromArgb(22, 27, 34);
+            UsernameTextBox.BorderStyle = BorderStyle.FixedSingle;
+            UsernameTextBox.ForeColor = Color.White;
+            UsernameTextBox.Location = new Point(73, 139);
+            UsernameTextBox.MaxLength = 16;
+            UsernameTextBox.Name = "UsernameTextBox";
+            UsernameTextBox.PlaceholderText = "Username";
+            UsernameTextBox.Size = new Size(244, 27);
+            UsernameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.BackColor = Color.FromArgb(22, 27, 34);
+            PasswordTextBox.BorderStyle = BorderStyle.FixedSingle;
+            PasswordTextBox.ForeColor = Color.White;
+            PasswordTextBox.Location = new Point(73, 213);
+            PasswordTextBox.MaxLength = 32;
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.PasswordChar = '*';
+            PasswordTextBox.PlaceholderText = "Password";
+            PasswordTextBox.Size = new Size(244, 27);
+            PasswordTextBox.TabIndex = 3;
+            // 
+            // TopLabel
+            // 
+            TopLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            TopLabel.AutoSize = true;
+            TopLabel.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            TopLabel.ForeColor = Color.White;
+            TopLabel.Location = new Point(0, 54);
+            TopLabel.MinimumSize = new Size(390, 0);
+            TopLabel.Name = "TopLabel";
+            TopLabel.Size = new Size(390, 31);
+            TopLabel.TabIndex = 4;
+            TopLabel.Text = "Registration";
+            TopLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // RepeatPasswordTextBox
+            // 
+            RepeatPasswordTextBox.BackColor = Color.FromArgb(22, 27, 34);
+            RepeatPasswordTextBox.BorderStyle = BorderStyle.FixedSingle;
+            RepeatPasswordTextBox.ForeColor = Color.White;
+            RepeatPasswordTextBox.Location = new Point(73, 287);
+            RepeatPasswordTextBox.MaxLength = 32;
+            RepeatPasswordTextBox.Name = "RepeatPasswordTextBox";
+            RepeatPasswordTextBox.PasswordChar = '*';
+            RepeatPasswordTextBox.PlaceholderText = "Repeat password";
+            RepeatPasswordTextBox.Size = new Size(244, 27);
+            RepeatPasswordTextBox.TabIndex = 5;
+            // 
+            // ErrorLabel
+            // 
+            ErrorLabel.AutoSize = true;
+            ErrorLabel.ForeColor = Color.Firebrick;
+            ErrorLabel.Location = new Point(70, 97);
+            ErrorLabel.Name = "ErrorLabel";
+            ErrorLabel.Size = new Size(0, 20);
+            ErrorLabel.TabIndex = 6;
+            // 
+            // LogInLinkLabel
+            // 
+            LogInLinkLabel.ActiveLinkColor = Color.DarkGray;
+            LogInLinkLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            LogInLinkLabel.AutoSize = true;
+            LogInLinkLabel.LinkColor = Color.White;
+            LogInLinkLabel.Location = new Point(73, 323);
+            LogInLinkLabel.Name = "LogInLinkLabel";
+            LogInLinkLabel.Size = new Size(182, 20);
+            LogInLinkLabel.TabIndex = 7;
+            LogInLinkLabel.TabStop = true;
+            LogInLinkLabel.Text = "Already have an account ?";
+            LogInLinkLabel.VisitedLinkColor = Color.White;
+            LogInLinkLabel.LinkClicked += LogInLinkLabel_LinkClicked;
+            // 
+            // BorderRightPanel
+            // 
+            BorderRightPanel.BackColor = Color.FromArgb(27, 35, 48);
+            BorderRightPanel.Dock = DockStyle.Right;
+            BorderRightPanel.Location = new Point(387, 42);
+            BorderRightPanel.Name = "BorderRightPanel";
+            BorderRightPanel.Size = new Size(3, 380);
+            BorderRightPanel.TabIndex = 8;
+            // 
+            // BorderLeftPanel
+            // 
+            BorderLeftPanel.BackColor = Color.FromArgb(27, 35, 48);
+            BorderLeftPanel.Dock = DockStyle.Left;
+            BorderLeftPanel.Location = new Point(0, 42);
+            BorderLeftPanel.Name = "BorderLeftPanel";
+            BorderLeftPanel.Size = new Size(2, 380);
+            BorderLeftPanel.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(27, 35, 48);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(2, 419);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(385, 3);
+            panel1.TabIndex = 10;
+            // 
+            // AuthorizationForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(13, 17, 23);
+            ClientSize = new Size(390, 422);
+            Controls.Add(panel1);
+            Controls.Add(BorderLeftPanel);
+            Controls.Add(BorderRightPanel);
+            Controls.Add(LogInLinkLabel);
+            Controls.Add(ErrorLabel);
+            Controls.Add(RepeatPasswordTextBox);
+            Controls.Add(TopLabel);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(UsernameTextBox);
+            Controls.Add(SubmitButton);
+            Controls.Add(TitleBarPanel);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AuthorizationForm";
+            Text = "AuthorizationForm";
+            Load += AuthorizationForm_Load;
+            Shown += AuthorizationForm_Shown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Components.TitleBarPanel TitleBarPanel;
+        private Button SubmitButton;
+        private TextBox UsernameTextBox;
+        private TextBox PasswordTextBox;
+        private Label TopLabel;
+        private TextBox RepeatPasswordTextBox;
+        private Label ErrorLabel;
+        private LinkLabel LogInLinkLabel;
+        private Panel BorderRightPanel;
+        private Panel BorderLeftPanel;
+        private Panel panel1;
+    }
+}
---
+++ Forms/AuthorizationForm.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wall.Entities;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Wall.Forms
+{
+    public partial class AuthorizationForm : Form
+    {
+
+        public MainForm ParentForm;
+
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+        public AuthorizationForm(string str)
+        {
+            InitializeComponent();
+            if (str == "Log in") LogInPaint();
+
+            else if (str == "Sing up") SignUpPaint();
+
+        }
+
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+
+
+            TitleBarPanel.HideMinimizeButton();
+            TitleBarPanel.ParentForm = this;
+            Button InvisibleAcceptButton = new Button();
+            InvisibleAcceptButton.Click += InvisibleAcceptButton_Click;
+            this.AcceptButton = InvisibleAcceptButton;
+
+            UsernameTextBox.Focus();
+        }
+        private void AuthorizationForm_Shown(object sender, EventArgs e)
+        {
+            UsernameTextBox.Focus();
+        }
+        private void LogInLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LogInPaint();
+        }
+
+        private void SignUpLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            SignUpPaint();
+        }
+
+
+        public void LogInPaint()
+        {
+            RepeatPasswordTextBox.Visible = false;
+            TopLabel.Text = "Log in";
+            this.Height = 355;
+            SubmitButton.Text = "Login";
+            SubmitButton.Click -= SignUpButton_Click;
+            SubmitButton.Click += LogInButton_Click;
+            LogInLinkLabel.Text = "Don't have an account ?";
+            LogInLinkLabel.LinkClicked -= LogInLinkLabel_LinkClicked;
+            LogInLinkLabel.LinkClicked += SignUpLinkLabel_LinkClicked;
+        }
+
+        public void SignUpPaint()
+        {
+            RepeatPasswordTextBox.Visible = true;
+            TopLabel.Text = "Sign up";
+            this.Height = 422;
+            SubmitButton.Text = "Registration";
+            SubmitButton.Click -= LogInButton_Click;
+            SubmitButton.Click += SignUpButton_Click;
+            LogInLinkLabel.Text = "Already have an account ?";
+            LogInLinkLabel.LinkClicked -= SignUpLinkLabel_LinkClicked;
+            LogInLinkLabel.LinkClicked += LogInLinkLabel_LinkClicked;
+        }
+
+        private async void SignUpButton_Click(object sender, EventArgs e)
+        {
+
+            if (UsernameTextBox.Text.Length <= 3)
+            {
+                ErrorLabel.Text = "Username must be longer than 2 characters";
+                return;
+            }
+
+
+            try
+            {
+                if (await ParentForm.MyHubConnection.InvokeAsync<bool>("CheckIsUsernameAvailable", UsernameTextBox.Text))
+                {
+                    ErrorLabel.Text = "This username already taken";
+                    return;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            if (PasswordTextBox.Text.Length <= 4)
+            {
+                ErrorLabel.Text = "Password must be longer than 3 characters";
+                return;
+            }
+            if (PasswordTextBox.Text != RepeatPasswordTextBox.Text)
+            {
+                ErrorLabel.Text = "Password mismatch";
+                return;
+            }
+
+
+            try
+            {
+                await ParentForm.MyHubConnection.InvokeAsync("RegisterNewUser", UsernameTextBox.Text, PasswordTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            LogIn();
+
+
+        }
+        private async void LogInButton_Click(object sender, EventArgs e)
+        {
+            if (UsernameTextBox.Text == String.Empty)
+            {
+                ErrorLabel.Text = "Username field can't be empty";
+                return;
+            }
+            if (PasswordTextBox.Text == String.Empty)
+            {
+                ErrorLabel.Text = "Password field can't be empty";
+                return;
+            }
+
+            LogIn();
+        }
+        private async void LogIn()
+        {
+            try
+            {
+                ChatUser newUser =
+                await ParentForm.MyHubConnection.InvokeAsync<ChatUser>("LogIn", UsernameTextBox.Text, PasswordTextBox.Text);
+
+                if (newUser != null)
+                {
+
+                    ParentForm.AuthorizedUser = newUser;
+
+                    ParentForm.UserLogIn(newUser.Username);
+
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void InvisibleAcceptButton_Click(object sender, EventArgs e)
+        {
+
+            if (ActiveControl == UsernameTextBox)
+            {
+                PasswordTextBox.Focus();
+                return;
+            }
+            if (ActiveControl == PasswordTextBox)
+            {
+
+                if (TopLabel.Text == "Log in")
+                {
+
+                    LogInButton_Click(this, new EventArgs());
+                    return;
+                }
+                else
+                {
+
+                    RepeatPasswordTextBox.Focus();
+                    return;
+                }
+            }
+            if (ActiveControl == RepeatPasswordTextBox && TopLabel.Text == "Sign up")
+            {
+
+                SignUpButton_Click(this, new EventArgs());
+                return;
+            }
+        }
+
+
+    }
+}
+
